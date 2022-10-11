32a4b40de4090c930f5788b509bad0544430b40f
Kyle Newkirk <kylednewkirk@gmail.com>
2022-10-11 16:10:33 -0700

    Add metamask connect and address fetching

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,14 +1,51 @@
-import Head from "next/head";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { WallyConnector } from "../../dist";
 
-import styles from "../styles/Home.module.css";
+import detectEthereumProvider from '@metamask/detect-provider';
+import { MetaMaskInpageProvider } from '@metamask/providers';
+
+import styles from "styles/Home.module.css";
+
+const MetaMaskInfo = (props: any) => {
+
+  const [provider, setProvider] = useState<MetaMaskInpageProvider>(null);
+  const [addr, setAddr] = useState(null);
+
+  useEffect(() => {
+    detectEthereumProvider().then(p => {
+      if (p) {
+        setProvider(p as MetaMaskInpageProvider);
+      }
+    });
+  }, [])
+
+  useEffect(() => {
+    if (provider && provider.selectedAddress) {
+      setAddr(provider.selectedAddress);
+    }
+  }, [provider])
+
+  if (!provider) {
+    return null;
+  }
+
+  return (
+    addr === null
+      ? <button onClick={() => {
+        provider.request({ method: 'eth_requestAccounts' }).then(res => {
+          console.log({ res });
+          setAddr(res[0]);
+        })
+      }}>Connect with MetaMask</button>
+      : <h2>Connected at {addr}</h2>
+  );
+}
 
 export default function Home() {
   const wallyConnector = useRef(
     new WallyConnector(
-      process.env.NEXT_PUBLIC_CLIENT_ID, 
+      process.env.NEXT_PUBLIC_CLIENT_ID,
       {
         isDevelopment: process.env.NEXT_PUBLIC_IS_DEVELOPMENT === 'true',
       }
@@ -22,30 +59,25 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Demo Page</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Wally Connector Demo</h1>
-        <button
-          onClick={() => {
-            wallyConnector.current.loginWithEmail();
-          }}
-        >
-          Login
-        </button>
-        <button
-          onClick={async () => {
-            console.log("stub");
-            // const wallets = await wallyConnector.current.getWallets();
-            // console.log("wallets = ", wallets);
-          }}
-        >
-          Get Wallets
-        </button>
-      </main>
-    </div>
+    <>
+      <h1 className={styles.title}>EasySign Demo</h1>
+      {typeof window !== 'undefined' ? <MetaMaskInfo /> : null}
+      <button
+        onClick={() => {
+          wallyConnector.current.loginWithEmail();
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={async () => {
+          console.log("stub");
+          // const wallets = await wallyConnector.current.getWallets();
+          // console.log("wallets = ", wallets);
+        }}
+      >
+        Get Wallets
+      </button>
+    </>
   );
 }
