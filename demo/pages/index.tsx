8b4d2940f7b1185ab873366c9f8ee071e73448e4
Kyle Newkirk <kylednewkirk@gmail.com>
2022-10-13 14:34:12 -0700

    Add switch for using wally & login button

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import detectEthereumProvider from '@metamask/detect-provider';
 import { MetaMaskInpageProvider } from '@metamask/providers';
 
@@ -11,11 +11,27 @@ import styles from "styles/Home.module.css";
 
 
 export default function Home() {
-  const [provider, setProvider] = useState<MetaMaskInpageProvider>(null);
+  const [isUsingWally, setIsUsingWally] = useState(true);
+  const [provider, setProvider] = useState<MetaMaskInpageProvider | any>(null);
   const [address, setAddress] = useState(null);
 
+  const detectProvider = useCallback(() => {
+    if (isUsingWally) {
+      return Promise.resolve(
+        new WallyConnector(
+          process.env.NEXT_PUBLIC_CLIENT_ID,
+          {
+            isDevelopment: process.env.NEXT_PUBLIC_IS_DEVELOPMENT === 'true',
+          }
+        )
+      )
+    } else {
+      return detectEthereumProvider();
+    }
+  }, [isUsingWally])
+
   useEffect(() => {
-    detectEthereumProvider().then(p => {
+    detectProvider().then(p => {
       if (p) {
         setProvider(p as MetaMaskInpageProvider);
       }
@@ -28,25 +44,28 @@ export default function Home() {
     }
   }, [provider])
 
-
-  const wallyConnector = useRef(
-    new WallyConnector(
-      process.env.NEXT_PUBLIC_CLIENT_ID,
-      {
-        isDevelopment: process.env.NEXT_PUBLIC_IS_DEVELOPMENT === 'true',
-      }
-    )
-  );
-
   useEffect(() => {
-    if (wallyConnector.current.isRedirected()) {
-      wallyConnector.current.handleRedirect()
+    if (isUsingWally && provider && provider.isRedirected()) {
+      provider.handleRedirect();
     }
-  }, []);
+  }, [isUsingWally, provider]);
+
+  const onWallyClick = () => {
+    provider.loginWithEmail()
+  };
+
+  const onChange = e => {
+    setIsUsingWally(e.target.value === "wally");
+  };
 
   return (
     <>
       <h1 className={styles.title}>EasySign Demo</h1>
+      <span>
+        Use <input type="radio" name="provider" value="metamask" checked={!isUsingWally} onChange={onChange}/>MetaMask
+        <input type="radio" name="provider" value="wally" checked={isUsingWally} onChange={onChange}/>Wally
+        {isUsingWally ? <button onClick={onWallyClick}>Login</button> : null }
+      </span>
       {address
         ? <Sign provider={provider} address={address}/>
         : <Connect
