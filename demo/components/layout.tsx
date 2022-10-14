e104af8d88b28a42280a6068211c87484c39ce92
Kyle Newkirk <kylednewkirk@gmail.com>
2022-10-13 17:56:03 -0700

    Update lint config, fix lint

--- demo/components/layout.tsx
+++ demo/components/layout.tsx
@@ -1,22 +1,21 @@
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 
-import styles from "styles/Home.module.css";
+import styles from 'styles/Home.module.css';
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children: any;
 }
 
-function Layout(props: LayoutProps) {
+const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Demo Page</title>
       </Head>
-      <main className={styles.main}>
-        {props.children}
-      </main>
+      <main className={styles.main}>{props.children}</main>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
