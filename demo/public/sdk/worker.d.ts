df6d9f8ed3d2687129cc4891b2b349167f01d7db
Kyle Newkirk <kylednewkirk@gmail.com>
2022-11-29 12:37:29 -0800

    fix after rebase

--- demo/public/sdk/worker.d.ts
+++
@@ -1,6 +0,0 @@
-interface SharedWorkerGlobalScope {
-    onconnect: (event: MessageEvent) => void;
-    ports: MessagePort[];
-}
-declare const _self: SharedWorkerGlobalScope;
-declare const broadcast: (e: MessageEvent) => void;
